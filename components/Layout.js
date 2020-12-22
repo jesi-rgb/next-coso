c3a1662eb89def2558f572f64ade5898f76fdd53
Jesús Enrique Rascón <blograso@gmail.com>
2020-12-22 17:18:38 +0100

    adding tailwind and api

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,16 @@ import { Header } from "./Header";
 export const Layout = ({ children }) => {
   return (
     <div>
+      <style jsx>{`
+        .container {
+          margin: 50px;
+        }
+        p {
+          color: blue;
+        }
+      `}</style>
       <Header />
+      <p>try header</p>
       {children}
       <Footer />
     </div>
--- pages/abdu.js
+++ pages/abdu.js
@@ -1,10 +1,18 @@
-import { Header } from "../components/Header";
+import useSWR from "swr";
 import { Layout } from "../components/Layout";
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 const Abdu = () => {
+  //   https://swapi.dev/api/starships/12/
+  const { data, error } = useSWR(
+    "https://swapi.dev/api/starships/12/",
+    fetcher
+  );
+
   return (
     <Layout>
-      <p>we on abdu</p>
+      <p className="text-2xl">{JSON.stringify(data)}</p>
     </Layout>
   );
 };
